17e53919e9358c854317456d5e5d06c7cfc2192d
Paola Garbato <paolagarbato@gmail.com>
2021-02-08 12:22:37 -0300

    Iniciando controller/view dos cursos, definindo como página inicial, igualando versões dos pacotes

---
+++ CursosMVC/Controllers/CursosController.cs
@@ -0,0 +1,95 @@
+﻿using CursosMVC.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CursosMVC.Controllers
+{
+    public class CursosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CursosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CursosController
+        public async Task<ActionResult> IndexAsync()
+        {
+            var cursos = _context.Cursos;
+
+            return View(await cursos.ToListAsync());
+        }
+
+        // GET: CursosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CursosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CursosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CursosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CursosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CursosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CursosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- CursosMVC/Startup.cs
+++ CursosMVC/Startup.cs
@@ -49,7 +49,7 @@ namespace CursosMVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Cursos}/{action=Index}/{id?}");
             });
         }
     }
